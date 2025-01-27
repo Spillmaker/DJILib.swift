c2f4c27251ab2eb1ce081676fe0e5307a648c7dd
Krister Berntsen <krister@spillmaker.no>
2025-01-27 04:57:17 +0100

    Removed duplicate function
    How did that get in there

--- Sources/DJILib/DJILib.swift
+++ Sources/DJILib/DJILib.swift
@@ -64,43 +64,6 @@ public class DJILib {
     public static let manufacturerDataOsmoPocket3Id        = Data([0x20, 0x00])
     
     
-    public static func getModelFromManufacturerData(manufacturerData: Data) -> DJILib.Model? {
-        
-        guard manufacturerData.count >= 4 else {
-            //print("ManufacturerData is shorter than expected.")
-            return nil
-        }
-        
-        let manufacturerID = manufacturerData[0 ... 1]
-        guard manufacturerID == DJILib.manufacturerDataIdentifier else {
-            //print("Device does not have DJI manufacturer id.")
-            return nil
-        }
-        //print("Pheripheral with DJI manufacturer id found.")
-        
-
-        let modelId = manufacturerData[2 ... 3]
-        
-        if(modelId == DJILib.manufacturerDataOsmoAction3Id){
-            return .oa3
-        }
-        
-        if(modelId == DJILib.manufacturerDataOsmoAction4Id){
-            return .oa4
-        }
-        
-        if(modelId == DJILib.manufacturerDataOsmoAction5ProId){
-            return .oa5pro
-        }
-        
-        if(modelId == DJILib.manufacturerDataOsmoPocket3Id){
-            return .op3
-        }
-        
-        
-        return nil
-        
-    }
     
     
     public static let commandCharacteristicsUUID           = Data([0xFF, 0xF4])
@@ -432,7 +395,7 @@ public class DJILib {
         print("DJILIB Initiated")
     }
     
-    static func getModelFromManufacturerData(manufacturerData: Data) -> Model? {
+    public static func getModelFromManufacturerData(manufacturerData: Data) -> Model? {
         
         guard manufacturerData.count >= 4 else {
             log("Recieved invalid manufacturer-data")
